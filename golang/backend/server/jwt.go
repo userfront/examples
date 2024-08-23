7e417ce4b9f2f12070ee93e856218d2208773a69
aPaladiychuk <andrey.paladiychuk@gmail.com>
2024-08-23 14:42:58 +0300

    run backend in ssl

--- golang/backend/server/jwt.go
+++ golang/backend/server/jwt.go
@@ -1,32 +1,40 @@
 package server
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"github.com/golang-jwt/jwt"
 )
 
 // Identity represents the identity of a user with JWT claims, access token, refresh token, and user information.
+
+type User struct {
+	jwt.StandardClaims
+	Authentication struct {
+		FirstFactor struct {
+			Channel  string `json:"channel"`
+			Strategy string `json:"strategy"`
+		} `json:"firstFactor"`
+	} `json:"authentication"`
+	Authorization struct {
+		N8Zwd9N struct {
+			Roles []string `json:"roles"`
+		} `json:"7n8zwd9n"`
+	} `json:"authorization"`
+	IsConfirmed            bool   `json:"isConfirmed"`
+	IsEmailConfirmed       bool   `json:"isEmailConfirmed"`
+	IsPhoneNumberConfirmed bool   `json:"isPhoneNumberConfirmed"`
+	Mode                   string `json:"mode"`
+	SessionId              string `json:"sessionId"`
+	TenantId               string `json:"tenantId"`
+	UserId                 int    `json:"userId"`
+	UserUuid               string `json:"userUuid"`
+}
 type (
-	Identity struct {
-		jwt.StandardClaims
-		Payload
-	}
-
-	Payload struct {
-		Mode          string                 `json:"mode"`
-		TenantId      string                 `json:"tenantId"`
-		UserId        int64                  `json:"userId"`
-		UserUuid      string                 `json:"userUuid"`
-		IsConfirmed   bool                   `json:"isConfirmed"`
-		Authorization map[string]interface{} `json:"authorization"`
-		Iss           string                 `json:"iss"`
-		SessionId     string                 `json:"sessionId"`
-		Iat           int64                  `json:"iat"`
-		Exp           int64                  `json:"exp"`
-	}
-
 	JWTService interface {
-		ParseAndValidate(string) (*Identity, error)
+		ParseAndValidate(string) (*User, error)
 	}
 	jwtService struct {
 		jwtSecret string `json:"jwt_secret"`
@@ -47,10 +55,11 @@ func NewJWTService(jwtSecret string) JWTService {
 // Returns:
 // - *Identity: a pointer to the parsed Identity if the token is valid.
 // - error: an error if parsing or validation fails.
-func (j *jwtService) ParseAndValidate(tokenString string) (*Identity, error) {
-	var identity Identity
+func (j *jwtService) ParseAndValidate(tokenString string) (*User, error) {
+	var identity User
+
 	token, err := jwt.ParseWithClaims(tokenString, &identity, func(token *jwt.Token) (interface{}, error) {
-		return []byte(j.jwtSecret), nil
+		return publicKey([]byte(j.jwtSecret))
 	})
 	if err != nil {
 		return nil, err
@@ -58,6 +67,21 @@ func (j *jwtService) ParseAndValidate(tokenString string) (*Identity, error) {
 	if !token.Valid {
 		return nil, fmt.Errorf("invalid token")
 	}
-
 	return &identity, nil
+}
+
+func publicKey(source []byte) (*rsa.PublicKey, error) {
+	pubPem, _ := pem.Decode(source)
+	if pubPem == nil {
+		return nil, fmt.Errorf("failed to decode PEM")
+	}
+	parsedKey, err := x509.ParsePKIXPublicKey(pubPem.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pubKey, ok := parsedKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("failed to parse public key")
+	}
+	return pubKey, nil
 }
